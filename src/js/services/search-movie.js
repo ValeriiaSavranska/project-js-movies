55f262acfa7c8bfc5c18482d4ca729e5e4ced316
ValeriiaSavranska <lerarog1997@gmail.com>
2021-11-26 17:58:33 +0200

    cteate api-services

---
+++ src/js/services/search-movie.js
@@ -0,0 +1,9 @@
+import { BASE_URL, API_KEY, page } from './variables-for-request';
+
+const getSearchMovie = async movieToSearch => {
+  const response = await fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${movieToSearch}`);
+  const data = await response.json();
+  return data;
+};
+
+getSearchMovie('venom').then(movies => console.log(movies));
