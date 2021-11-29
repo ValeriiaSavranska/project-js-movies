48ddb5a6d53f4a79a02db3811856b5ba98643d9e
ValeriiaSavranska <lerarog1997@gmail.com>
2021-11-29 13:33:45 +0200

    new-api-services

--- src/js/partials-js/main-page.js
+++ src/js/partials-js/main-page.js
@@ -1,21 +1,18 @@
-import { getTrendingMovies } from '../services/get-trending-movies';
-import { getGenres } from '../services/get-genres';
-import createMarkup from '../markup/markup-gallery'
+import { getGenres, getTrendingMovies } from '../services/api-services';
+import createMarkup from '../markup/markup-gallery';
 
 const galleryDiv = document.querySelector('.gallery');
 const headerNavTitle = document.querySelector('.header-nav__logo');
 
-const renderMovies = (movies) => {
+const renderMovies = movies => {
   const markup = createMarkup(movies);
   galleryDiv.innerHTML = markup;
-}
+};
 getGenres()
   .then(getTrendingMovies)
   .then(movies => renderMovies(movies));
 
 //getTrendingMovies().then(movies => markupTrendin(movies));
-
-
 
 headerNavTitle.addEventListener('click', e => {
   e.preventDefault();
--- src/js/services/variables-for-request.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '61e2735d71b7e3847b723f570f360fd1';
-let page = 1;
-
-export { BASE_URL, API_KEY, page };
