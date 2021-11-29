dfd20192c7f89304fd73484b3c12db3244c16b4d
bod111 <86015208+bod111@users.noreply.github.com>
2021-11-29 21:35:26 +0200

    fix2

--- src/js/markup/markup-gallery.js
+++ src/js/markup/markup-gallery.js
@@ -3,14 +3,14 @@ import { genres } from '../services/api-services';
 export default function createMarkup(movies) {
   const markup = movies.results.map(
     ({ id, poster_path, backdrop_pathL, genre_ids, release_date, title }) => {
-      let arrGenre = genre_ids.map(id => genres.find(genre => genre.id === id).name);
+      let arrGenre = genre_ids.map(id => genres.find(genre => genre.id === id)?.name ?? 'Other');
       let img = `https://image.tmdb.org/t/p/w300${poster_path}`;
       if (arrGenre.length > 3) {
         arrGenre.splice(2, arrGenre.length - 2, 'Other');
       }
       const year = new Date(release_date);
       return `
-        <li class="card" id="${id}>
+        <li class="card">
         <img class="card-img" src="${
           !poster_path
             ? 'https://images.squarespace-cdn.com/content/v1/51cdafc4e4b09eb676a64e68/1470175715831-NUJOMI6VW13ZNT1MI0VB/image-asset.jpeg?format=750w'
