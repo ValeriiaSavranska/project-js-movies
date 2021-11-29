fc2ff441367db481f19565300293ff3a2d09ade7
DartRock <64318620+DartRock@users.noreply.github.com>
2021-11-29 10:54:59 +0200

    fixed logo

--- src/js/partials-js/header.js
+++ src/js/partials-js/header.js
@@ -1,15 +1,18 @@
 const header = document.querySelector('#head');
 const homePage = document.querySelector('#home-page');
 const libraryPage = document.querySelector('#library-page');
-const headerNav = document.querySelector('.header-nav__list');
+const headerNav = document.querySelector('.header-nav');
 const homePageWrapper = document.querySelector('.search-film__wrap');
 const libraryPageWrapper = document.querySelector('.my-library__wrap');
+const navLogo = document.querySelector('#nav-logo');
+const logoIcon = document.querySelector('.header-nav__icon');
 
 const changePage = e => {
   e.preventDefault();
+
   if (e.target === e.currentTarget) return;
 
-  if (e.target === homePage) {
+  if (e.target === homePage || e.target === navLogo || e.target === logoIcon) {
     libraryPageWrapper.classList.add('visually-hidden');
     homePageWrapper.classList.remove('visually-hidden');
     libraryPage.classList.remove('current');
@@ -18,8 +21,8 @@ const changePage = e => {
   }
 
   if (e.target === libraryPage) {
-    homePageWrapper.classList.add('visually-hidden');
     libraryPageWrapper.classList.remove('visually-hidden');
+    homePageWrapper.classList.add('visually-hidden');
     libraryPage.classList.add('current');
     homePage.classList.remove('current');
     header.classList.add('header__main--library-bcg');
