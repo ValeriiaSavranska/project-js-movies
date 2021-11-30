ed1405b1f1bbe580058184bcb4b9778578571969
spelui <vlad.spelui@gmail.com>
2021-11-30 11:40:50 +0200

    add up btn

--- src/js/partials-js/up_btn.js
+++ src/js/partials-js/up_btn.js
@@ -1,31 +1,20 @@
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// const btn = $('#up-button');
-
-// $(window).scroll(function () {
-//   if ($(window).scrollTop() > 700) {
-//     btn.addClass('show');
-//   } else {
-//     btn.removeClass('show');
-//   }
-// });
-
-// btn.on('click', function (e) {
-//   e.preventDefault();
-//   $('html, body').animate({ scrollTop: 0 }, '300');
-// });
-
 const upBtnRef = document.querySelector('#up-button');
 
 const onScrollbottom = () => {
-  //   console.log(window.scrollTop);
-  if (window.pageYOffset > 200) {
-    upBtnRef.classList.add('show');
+  if (window.scrollY > 200) {
+    upBtnRef.classList.add('visible');
   } else {
-    upBtnRef.classList.remove('show');
+    upBtnRef.classList.remove('visible');
   }
 };
 
 window.addEventListener('scroll', onScrollbottom);
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
+upBtnRef.addEventListener('click', scrollToTop);
