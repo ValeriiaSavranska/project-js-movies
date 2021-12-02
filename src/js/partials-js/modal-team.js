62067f7d41fb4b4593bcd119db9950fbba563a4c
ValeriiaSavranska <lerarog1997@gmail.com>
2021-12-02 09:40:27 +0200

    fix6

--- src/js/partials-js/modal-team.js
+++ src/js/partials-js/modal-team.js
@@ -3,7 +3,7 @@ import imgVladyslav from '../../images/team/vladyslav.jpg';
 import imgBogdan from '../../images/team/bogdan.jpg';
 import imgDima from '../../images/team/dima.jpg';
 import imgIvan from '../../images/team/ivan.jpg';
-import imgValentyn from '../../images/team/valentyn.jpg';
+// import imgValentyn from '../../images/team';
 import imgVitalii from '../../images/team/vitalii.jpg';
 
 import SimpleLightbox from 'simplelightbox';
@@ -56,13 +56,6 @@ function createModalTeam() {
     <img src="${imgIvan}"  class="img-team" alt="Ivan Kostenko" width="150"/>
   </a>
  <p class="name-team">Ivan Kostenko</p>
-  <p class="pos-team">Developer</p>
-  </li>
-  <li class="modal-body__item team-leader">
-  <a class="modal-body__item" href="${imgValentyn}">
-    <img src="${imgValentyn}"  class="img-team" alt="Valentyn Onyshchenko" width="150"/>
-  </a>  
- <p class="name-team">Valentyn Onyshchenko</p>
   <p class="pos-team">Developer</p>
   </li>
   <li class="modal-body__item team-leader">
